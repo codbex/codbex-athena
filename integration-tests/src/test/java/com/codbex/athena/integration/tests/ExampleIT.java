0b90fb06c34b689a2aa8d9765cf3c0b4b2f419ac
alt-plus-f4 <valentin.assenov@abv.bg>
2025-04-25 13:16:41 +0300

    fix: tests

--- integration-tests/src/test/java/com/codbex/athena/integration/tests/ExampleIT.java
+++ integration-tests/src/test/java/com/codbex/athena/integration/tests/ExampleIT.java
@@ -1,5 +1,6 @@
 package com.codbex.athena.integration.tests;
 
+import org.eclipse.dirigible.tests.framework.browser.HtmlElementType;
 import org.junit.jupiter.api.Test;
 
 
@@ -7,6 +8,11 @@ class ExampleIT extends IntegrationTest {
 
     @Test
     void test() {
+        openHomepage();
+    }
 
+    private void openHomepage(){
+        ide.openPath("/services/web/dashboard");
+//        browser.assertElementExistsByTypeAndContainsText(HtmlElementType.HEADER1, "Athena");
     }
 }
