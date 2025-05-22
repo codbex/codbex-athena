c21e51160da2df5828c010b718b13d38dae0724e
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-05-22 09:25:13 +0300

    add comment for the reload issue

--- integration-tests/src/test/java/com/codbex/athena/integration/tests/application/SalesInvoiceIT.java
+++ integration-tests/src/test/java/com/codbex/athena/integration/tests/application/SalesInvoiceIT.java
@@ -27,6 +27,7 @@ class SalesInvoiceIT extends ApplicationIntegrationTest {
         browser.openPath("/");
         ide.login(false);
 
+        // TODO: remove the reload once https://github.com/eclipse-dirigible/dirigible/issues/5032 is fixed
         createCity();
         ide.reload();
 
